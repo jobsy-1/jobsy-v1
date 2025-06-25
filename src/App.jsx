f64f5423655e905d29f2f4e5046eb7643fbd7978
Arya Hemin <ariahemin@gmail.com>
2025-06-26 02:18:52 +0300

    stupid changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Changed BrowserRouter to HashRouter
 import LandingPage from './pages/LandingPage';
 import SignUpPage from './pages/SignUpPage';
 import CompleteProfilePage from './pages/CompleteProfilePage';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import EditProfilePage from './pages/EditProfilePage';
-
+// Removed: AuthCallbackPage import as it's no longer needed for manual OTP
 
 function App() {
   return (
-    <Router>
+    <Router> {/* Now using HashRouter */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUpPage />} />
